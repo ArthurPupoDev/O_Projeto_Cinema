0a19087ae20bf5553a33f205e01e1ec8929c667d
ArthurPupoDev <84886409+ArthurPupoDev@users.noreply.github.com>
2024-06-22 20:40:43 -0300

    second

---
+++ src/main/java/br/edu/up/o_projeto_cinema/model/Atendente.java
@@ -0,0 +1,8 @@
+package br.edu.up.o_projeto_cinema.model;
+
+public class Atendente extends Funcionario{
+
+    public Atendente(String nome, String cpf, String dataNascimento, Float salario){
+        super(nome, cpf, dataNascimento,salario);
+    }
+}
